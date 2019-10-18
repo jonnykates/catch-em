6fbd5b5a37fc8a40ec735990ae007f1f7ed258f7
Jonny Kates <jonnykates@gmail.com>
2019-10-18 08:24:51 +0100

    Fetch original 150 pokemon for list

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import PokemonList from './PokemonList';
 
 function App() {
-  return (
-    <p>Hi</p>
-  );
+  const [pokemon, setPokemon] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://pokeapi.co/api/v2/pokemon?limit=150').then(res => {
+      setPokemon(res.data.results.map(pokeman => pokeman.name));
+    });
+  }, []);
+
+  return <PokemonList pokemon={pokemon} />;
 }
 
 export default App;
---
+++ src/PokemonList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function PokemonList({ pokemon }) {
+  return (
+    <div>
+      {pokemon.map(pokeman => (
+        <div key={pokeman}>{pokeman}</div>
+      ))}
+    </div>
+  )
+}
