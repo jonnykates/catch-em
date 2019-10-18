094de88626face000e52a462f5ae17e905b95586
Jonny Kates <jonnykates@gmail.com>
2019-10-18 16:54:08 +0100

    List to class component, add search filter, refactor sprite image source

--- src/PokemonList.js
+++ src/PokemonList.js
@@ -1,24 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-export default function PokemonList({ pokemon }) {
-  return (
-    <ul>
-      {pokemon.map((pokeman, index) => (
-        <li key={pokeman}>
-          <Link to={pokeman}>
-            <img
-              src={
-                'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/' +
-                (index + 1) +
-                '.png'
-              }
-              alt={pokeman}
-            />
-            <h4>{pokeman}</h4>
-          </Link>
-        </li>
-      ))}
-    </ul>
-  );
+export default class PokemonList extends Component {
+  constructor() {
+    super();
+    this.state = {
+      searchTerm: ''
+    };
+  }
+
+  updateSearch = e => {
+    this.setState({ searchTerm: e.target.value });
+  };
+
+  render() {
+    let filteredPokemon = this.props.pokemon.filter(pokeman => {
+      return (
+        pokeman.toLowerCase().indexOf(this.state.searchTerm.toLowerCase()) !==
+        -1
+      );
+    });
+
+    return (
+      <>
+        <div className="list-search">
+          <input
+            type="text"
+            label="Search Pokemon"
+            value={this.state.search}
+            onChange={this.updateSearch}
+          />
+        </div>
+        <ul className="pokemon-list">
+          {filteredPokemon.map(pokeman => (
+            <li key={pokeman}>
+              <Link to={pokeman}>
+                <img
+                  src={
+                    'https://img.pokemondb.net/sprites/omega-ruby-alpha-sapphire/dex/normal/' +
+                    pokeman +
+                    '.png'
+                  }
+                  alt={pokeman}
+                />
+                <h4>{pokeman}</h4>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </>
+    );
+  }
 }
